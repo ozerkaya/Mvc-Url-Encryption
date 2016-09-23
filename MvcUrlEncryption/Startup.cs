012826363f7e566c3db7488908e725b05dd26404
ozer.kaya <ozer.kaya@bimsa.com.tr>
2016-09-23 11:05:38 +0300

    First Commit

---
+++ MvcUrlEncryption/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcUrlEncryption.Startup))]
+namespace MvcUrlEncryption
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
